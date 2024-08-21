bdfadf164945a8e5bc0b5e7fd697d6ee88906588
AppleNote <a7201492425@gmail.com>
2024-08-21 12:51:08 +0900

    2024.08.21. 12:51

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import './index.css';
 import Senicare from './Senicare';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Senicare />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Senicare />
+    </React.StrictMode>
 );
