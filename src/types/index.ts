b9023b4dffc55687d474ea804b451f333abc2299
AppleNote <a7201492425@gmail.com>
2024-09-27 17:39:13 +0900

    2024.09.27. 17:39

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,12 @@ import Tool from "./tool.interface";
 import SignInUser from "./sign-in-user.interface";
 import Customer from "./customer.interface";
 import Nurse from "./nurse.interface";
+import CareRecord from "./care-record.interface";
 
 export type {
     Tool,
     SignInUser,
     Customer,
-    Nurse
+    Nurse,
+    CareRecord
 }
